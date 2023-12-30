2e9f5d56bf438206f6738de77e524ac19b4ddc1c
masyrnegames <masyrne.mc@gmail.com>
2023-12-30 14:24:41 +0300

    feat: Added semantic-release

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Романова Ильи";
+  const greeting = "Романов Илья";
 
-
+  // console.log('This is App ');
   return (
     <div className="App">
      <header className="App-header">
