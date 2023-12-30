2e6ee76fbfb325ddd205cdc6955faff59d410187
masyrnegames <masyrne.mc@gmail.com>
2023-12-30 14:35:09 +0300

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Романов Илья";
+  const greeting = "Это тестовый проект";
 
   // console.log('This is App ');
   return (
